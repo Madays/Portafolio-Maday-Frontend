1eadf1fd133a2bb71fe2b3023d56dcd6b42f271e
Madays <maday.choque@gmail.com>
2017-04-07 17:08:06 -0500

    commint inicial

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    move("myBarHTML5",90);
+    move("myBarCss",90);    
+    move("myBarJs",70);
+    move("myBarJquery",70);
+    move("myBarSass",80);
+    move("myBarGithub",70);
+    move("myBarGulp",70);
+    
+    move("myBarProactividad",98);
+    move("myBarEquipo",90);
+    move("myBarLiderazgo",70);
+    move("myBarResponsabilidad",90);
+    move("myBarEntuciasmo",90);
+    move("myBarPensamiento",80);
+    move("myBarPuntualidad",80);
+});
+$('i.fa').click(function(){
+    $(this).toggleClass('fa-bars');
+    $('#menu').toggleClass('hidden');
+    $('#menu-xs-sm').toggleClass('hidden');
+});
+
+$('#link-skills').click(function(){
+    move("myBarHTML5",90);
+    move("myBarCss",90);    
+    move("myBarJs",70);
+    move("myBarJquery",70);
+    move("myBarSass",80);
+    move("myBarGithub",70);
+    move("myBarGulp",70);
+    
+    move("myBarProactividad",98);
+    move("myBarEquipo",90);
+    move("myBarLiderazgo",70);
+    move("myBarResponsabilidad",90);
+    move("myBarEntuciasmo",90);
+    move("myBarPensamiento",80);
+    move("myBarPuntualidad",80);
+});
+
+function move(_id,_valor) {
+  var elem = document.getElementById(_id);   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= _valor) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+    console.log(elem.textContent);
+}
+
